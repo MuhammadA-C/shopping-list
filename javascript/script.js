025877386963eebbf462da3f7bd8cfafe3f3c1d5
MuhammadA-C <phantomganhdi@gmail.com>
2023-08-01 15:52:42 -0700

    Dynamically Create List Items
    Created the JavaScript code to dynamically create the list items.

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+
+
+let ul = document.querySelector("ul");
+
+let listItem = document.createElement("li");
+let listItemText = document.createTextNode("Item Test");
+
+let listItemRemoveBtn = document.createElement("button");
+let addClass = "remove-item-btn";
+let btnType = "button";
+let btnText = document.createTextNode("X");
+
+///////////////////////////////////////////////////////////////
+
+//adds button type to button
+listItemRemoveBtn.type = btnType;
+//adds class to button
+listItemRemoveBtn.classList.add(addClass);
+//adds text to button
+listItemRemoveBtn.appendChild(btnText);
+
+//adds text to list item
+listItem.appendChild(listItemText);
+//adds button to list item
+listItem.appendChild(listItemRemoveBtn);
+
+//adds button to unordered list
+ul.appendChild(listItem);
